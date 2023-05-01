5fb8abed6d5cad442a0446ed7d2b0c6b4e7cbe71
sadirul islam <sadirul.islam786@gmail.com>
2023-05-01 21:43:58 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import {
+  View, Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default App;
